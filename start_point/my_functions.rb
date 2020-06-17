0aa91ab5e99b2c6f7eab0973653daf880bc3e7ed
Alina <lindenalina.al@gmail.com>
2020-06-17 14:39:33 +0100

    array_length and array_sum passed

---
+++ start_point/my_functions.rb
@@ -0,0 +1,13 @@
+def add_array_lengths(array1, array2)
+    total = 0
+    arrays_length = array1.length + array2.length
+    total += arrays_length
+end
+
+def sum_array(arr)
+ total = 0
+ for number in arr
+    total += number
+ end
+ return total
+end
